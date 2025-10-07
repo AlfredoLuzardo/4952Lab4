f98a07e44c3c1fdbac694b4787139cf713fc82f5
AlfredoLuzardo <luzardoneto2005@gmail.com>
2025-10-06 22:36:37 -0700

    Added server files

---
+++ Program.cs
@@ -0,0 +1,30 @@
+using Microsoft.EntityFrameworkCore;
+using ServerMCP.Data;
+
+var builder = WebApplication.CreateBuilder(args);
+
+builder.Services.AddMcpServer()
+.WithHttpTransport()
+.WithToolsFromAssembly();
+
+var connStr = builder.Configuration.GetConnectionString("DefaultConnection") 
+    ?? throw new InvalidOperationException("Connection string 'DefaultConnection' not found.");
+    
+builder.Services.AddDbContext<ApplicationDbContext>(
+	option => option.UseSqlite(connStr)
+);
+
+var app = builder.Build();
+
+// Add MCP Middleware
+app.MapMcp();
+
+// Apply database migrations on startup
+using (var scope = app.Services.CreateScope()) {
+    var services = scope.ServiceProvider;
+
+    var context = services.GetRequiredService<ApplicationDbContext>();    
+    context.Database.Migrate();
+}
+
+app.Run();
